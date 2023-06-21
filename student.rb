7ffda9305877211b505570da7091fc4d71718fd3
Daniel <carreraprogrammer@gmail.com>
2023-06-21 15:40:01 -0500

    Create relationships between Student and Classroom classes

--- student.rb
+++ student.rb
@@ -4,6 +4,7 @@ class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    classroom.students.push(self)
   end
 
   def play_hooky
