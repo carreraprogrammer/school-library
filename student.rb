51f074c69210af19f3b8edb651add3ae6564505b
Daniel <carreraprogrammer@gmail.com>
2023-06-20 20:28:35 -0500

    Add teacher class

--- student.rb
+++ student.rb
@@ -1,8 +1,6 @@
 require './person'
 
 class Student < Person
-  attr_accessor :specialization
-
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
