20d0beb797c671149f7418ca559326f634c966c7
Daniel <carreraprogrammer@gmail.com>
2023-06-21 15:45:46 -0500

    Add Book class

--- student.rb
+++ student.rb
@@ -1,13 +1,19 @@
 require './person'
 
 class Student < Person
+  attr_accessor :specialization, :classroom
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
-    classroom.students.push(self)
+    @classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def add_classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
