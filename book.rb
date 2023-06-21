3a866533773c1f5cf1d7681eaef59770aa29ed08
Daniel <carreraprogrammer@gmail.com>
2023-06-21 15:59:35 -0500

    Add has-many and belongs-to relations between Book and Rental classes

--- book.rb
+++ book.rb
@@ -1,8 +1,15 @@
+require './rental'
+
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def rent_book(date)
+    Rental.new(date, self)
   end
 end
--- rental.rb
+++ rental.rb
@@ -1,7 +1,13 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :book
 
-  def initialize(date)
+  def initialize(date, book)\
+    raise 'Book should be an instance of Book class' unless book.is_a?(Book)
+    raise 'Person should be an instance of Person class' unless person.is_a?(Person)
+    raise 'The book already was rented in that date' if book.rentals.map(&:date).include?(date)
+
     @date = date
+    @book = book
+    book.rentals.push(self)
   end
 end
