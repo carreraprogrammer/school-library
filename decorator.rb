b155ebb62df98f0128adbd43a4cd0e9ee0802abd
Daniel <carreraprogrammer@gmail.com>
2023-06-21 11:17:28 -0500

    Implement correct_name method on Decorator

--- decorator.rb
+++ decorator.rb
@@ -1,4 +1,12 @@
 require './nameable'
 
 class Decorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+    super()
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
 end
