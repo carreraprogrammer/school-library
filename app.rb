35f4cbd960809018cbcf6f61b6de90b3da2c2b6c
Daniel <carreraprogrammer@gmail.com>
2023-06-22 21:55:56 -0500

    Refactorize and fix linters

--- app.rb
+++ app.rb
@@ -13,30 +13,56 @@ class App
   def select_option(user_response)
     case user_response
     when 1
-      @book_options.list_all_books
+      list_all_books
     when 2
-      @person_options.list_all_people
+      list_all_people
     when 3
-      print 'Do you want to create a student (1) or a teacher (2) ? [Input the number]: '
-      select_person = gets.chomp.to_i
-      case select_person
-      when 1
-        @person_options.create_a_student
-      when 2
-        @person_options.create_a_teacher
-      end
+      create_person
     when 4
-      print 'Title: '
-      title = gets.chomp
-      print 'Author: '
-      author = gets.chomp
-      @book_options.create_new_book(title, author)
+      create_book
     when 5
-      @rental_options.create_a_rental
+      create_rental
     when 6
-      @rental_options.rental_by_id
+      rental_by_id
     else
       puts "\nYou have selected #{user_response}"
     end
+  end
+
+  private
+
+  def list_all_books
+    @book_options.list_all_books
+  end
+
+  def list_all_people
+    @person_options.list_all_people
+  end
+
+  def create_person
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    select_person = gets.chomp.to_i
+    case select_person
+    when 1
+      @person_options.create_a_student
+    when 2
+      @person_options.create_a_teacher
+    end
+  end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    @book_options.create_new_book(title, author)
+  end
+
+  def create_rental
+    @rental_options.create_a_rental
+  end
+
+  def rental_by_id
+    @rental_options.rental_by_id
   end
 end
--- rental_options.rb
+++ rental_options.rb
@@ -11,34 +11,54 @@ class RentalOptions
 
   def create_a_rental
     puts "\nPlease, select a book from the following list by number: "
+    list_all_books
+    selected_book = gets.chomp.to_i
+
+    puts "\nSelect a person from the following list by number (not id)"
+    list_all_people
+    selected_person = gets.chomp.to_i
+
+    print 'Date: '
+    date = gets.chomp
+
+    create_rental(selected_book, selected_person, date)
+  end
+
+  def rental_by_id
+    print 'ID of person: '
+    person_id = gets.chomp.to_i
+    filtered_rentals = @rentals.filter { |rental| rental.person.id == person_id }
+    puts 'Rentals: '
+    filtered_rentals.each { |rental| puts "Date: #{rental.date}, Book #{rental.book.title} by #{rental.book.author}" }
+  end
+
+  private
+
+  def list_all_books
     counter_books = 0
     @books.books.each do |book|
       puts "#{counter_books}) Title: \"#{book.title}\", Author: #{book.author}"
       counter_books += 1
     end
-    selected_book = gets.chomp.to_i
-    puts "\nSelect a person from the following list by number (not id)"
+  end
+
+  def list_all_people
     counter_people = 0
     @people.people.each do |person|
       if person.is_a?(Student)
-        puts "#{counter_people}) [Student] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}, classroom: #{person.classroom}"
+        puts "#{counter_people}) [Student] Name: #{person.name}, ID: #{person.id}, Age: #{person.age},
+        classroom: #{person.classroom}"
       else
         puts "#{counter_people}) [Teacher] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
       end
       counter_people += 1
     end
-    selected_person = gets.chomp.to_i
-    print 'Date: '
-    date = gets.chomp
-    @rentals.push(Rental.new(date, @books.books[selected_book], @people.people[selected_person]))
-    puts 'Rental created succeesfully'
   end
 
-  def rental_by_id
-    print "ID of person: "
-    person_id = gets.chomp.to_i
-    filtered_rentals = @rentals.filter { |rental| rental.person.id == person_id}
-    puts "Rentals: " 
-    filtered_rentals.each { |rental| puts "Date: #{rental.date}, Book #{rental.book.title} by #{rental.book.author}"}
+  def create_rental(book_index, person_index, date)
+    book = @books.books[book_index]
+    person = @people.people[person_index]
+    @rentals.push(Rental.new(date, book, person))
+    puts 'Rental created successfully'
   end
 end
