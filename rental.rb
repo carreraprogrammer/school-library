d7dba3d90151f781675a28f02fb17e5a1167962f
Daniel <carreraprogrammer@gmail.com>
2023-06-29 11:17:38 -0500

    Add rental tests

--- rental.rb
+++ rental.rb
@@ -5,15 +5,10 @@ class Rental
     raise 'Book should be an instance of Book class' unless book.is_a?(Book)
     raise 'The book already was rented in that date' if book.rentals.map(&:date).include?(date)
     raise 'Person should be an instance of Person class' unless person.is_a?(Person)
-
     @date = date
     @book = book
     @person = person
     book.rentals.push(self)
     person.rentals.push(self)
-  end
-
-  def add_rental(person, date)
-    Rental.new(date, self, person)
   end
 end
